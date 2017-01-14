1ba085808104ae74f8a697eee3f86bc8b6784ed9
FionaCLin <clin4614@uni.sydney.edu.au>
2017-01-14 15:12:19 +1100

    set up task testGraph.c

---
+++ testGraph.c
@@ -0,0 +1,16 @@
+/*
+ *Task 0: Graph Black-Box Unit Testing 4 marks
+ * You must write a testGraph.c that tests the functions defined in the
+ * given Graph.h file. You must create assert(3)-based tests. We run
+ * your testGraph.c code with our own working and broken graph
+ * implementations to check they pass and fail the implementations
+ * accordingly.
+ *
+ * Only use and test functions from the original Graph.h. Do not need to
+ * submit tests for any additional functions you add to your graph when
+ * you complete the other tasks of the assignment.
+ *
+ * You do not need assert(3)-based tests for the functions show or
+ * destroyGraph.
+ */
+
