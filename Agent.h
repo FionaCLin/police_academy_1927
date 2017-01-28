b7f073da9fa8ba8d31b65ada6835493897582f65
FionaCLin <clin4614@uni.sydney.edu.au>
2017-01-28 20:12:38 +1100

    Implement winning and losing the game logic

---
+++ Agent.h
@@ -0,0 +1,32 @@
+//An ADT interface to represent an agent
+
+//Constants to represent search strategies used by the agents
+
+#define STATIONARY -1 //useful for debugging
+#define RANDOM 0
+#define C_L_VISITED 1 //Cheapest Least Visisted
+#define DFS 2
+
+
+typedef struct agentRep * Agent;
+
+//Initialise an agent
+Agent initAgent(Vertex start, int maxCycles,int stamina,
+                              int strategy,Graph g, char * name);
+void setDestination(Agent a, int end);
+
+//Calculate the next move
+Edge getNextMove(Agent agent,Graph graph);
+//Execute a given move
+void makeNextMove(Agent agent,Edge move);
+
+
+//Get information from the agent
+Vertex getCurrentLocation(Agent agent);
+Vertex getDestination(Agent agent);
+Vertex getStartLocation(Agent agent);
+char * getName(Agent agent);
+
+void printAgent(Agent agent);
+
+void destroyAgent(Agent agent);
--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 /**
-   main.c
+  main.c
 
-   The client game. This reads in data from a file to create a graph that
-   represents a map of cities.
-   It reads in data to create 5 agents: a thief and 4 detectives.
-   It then runs a simulation where the thief and detectives move
-   through the cities.
-   Currently only a random movement strategy has been implemented.
-   Stamina requirements and finishing a game need to implemented, as well as printing
-   out agent information and storing city names
-**/
+  The client game. This reads in data from a file to create a graph that
+  represents a map of cities.
+  It reads in data to create 5 agents: a thief and 4 detectives.
+  It then runs a simulation where the thief and detectives move
+  through the cities.
+  Currently only a random movement strategy has been implemented.
+  Stamina requirements and finishing a game need to implemented, as well as printing
+  out agent information and storing city names
+ **/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -69,15 +69,15 @@ Graph readGraph(char * filename) {
    char informant;
    char name[MAXLINE];
    while (counter < numV) {
-     fscanf(fp,"%d",&city);
-     counter++;
+      fscanf(fp,"%d",&city);
+      counter++;
 
-     while(fscanf(fp, "%d %d", &dest,&weight) == 2){
+      while(fscanf(fp, "%d %d", &dest,&weight) == 2){
          insertE(g,mkEdge(city, dest, weight));
-     }
-     //TASK - YOU MUST STORE THIS INFO IN THE GRAPH
-     fscanf(fp,"%c %[^\n]",&informant,name);
-
+      }
+      //TASK - YOU MUST STORE THIS INFO IN THE GRAPH
+      fscanf(fp,"%c %[^\n]",&informant,name);
+      readCityInfo(city, informant, name);
    }
    fclose(fp);
    return g;
@@ -87,159 +87,176 @@ Graph readGraph(char * filename) {
 // You may need to modify this function, to initialise all
 // important agent information
 void initialiseAgents(char *filename, Agent agents[], int maxCycles, Graph g){
-    FILE *fp;
-    fp = fopen (filename, "r"); // open data file
-    assert (fp != NULL);
-    int stamina;
-    Vertex start;
-    Vertex end;
-    char name[5];
-    int i;
-    int strategy;
+   FILE *fp;
+   fp = fopen (filename, "r"); // open data file
+   assert (fp != NULL);
+   int stamina;
+   Vertex start;
+   Vertex end;
+   char name[5];
+   int i;
+   int strategy;
 
-    fscanf(fp, "%d %d %d %[^\n]", &stamina,&start,&end,name);
-    agents[THIEF] = initAgent(start,maxCycles,stamina,RANDOM,g,name);
+   fscanf(fp, "%d %d %d %[^\n]", &stamina,&start,&end,name);
+   agents[THIEF] = initAgent(start,maxCycles,stamina,RANDOM,g,name);
+   setDestination(agents[THIEF], end);
 
-    for(i=1; i<=NUM_DETECTIVES; i++){
-        fscanf(fp, "%d %d %d %[^\n]", &stamina,&start,&strategy,name);
-        agents[i] = initAgent(start,maxCycles,stamina,strategy,g,name);
-    }
-    fclose(fp);
+   for(i=1; i<=NUM_DETECTIVES; i++){
+      fscanf(fp, "%d %d %d %[^\n]", &stamina,&start,&strategy,name);
+      agents[i] = initAgent(start,maxCycles,stamina,strategy,g,name);
+   }
+   fclose(fp);
 }
 
 //Display information about the current state of the game
 void display(int cycle,Agent agents[],Graph g){
-    int i;
-    printf("Hour %d\n",cycle);
-    // function to print the current location of each agent
-    printf ("  T  D1  D2  D3  D4\n");
-    for (i = 0; i <= NUM_DETECTIVES; i++) {
-	 printf ("%3d", getCurrentLocation(agents[i]));
-         printf(" ");
-    }
-    printf ("\n\n");
+   int i;
+   printf("Hour %d\n",cycle);
+   // function to print the current location of each agent
+   printf ("  T  D1  D2  D3  D4\n");
+   for (i = 0; i <= NUM_DETECTIVES; i++) {
+      Vertex city = getCurrentLocation(agents[i]);
+      printf ("%3d%s", city, isInformant(city));
+      printf (" ");
+   }
+   printf ("\n\n");
 }
 
 
 //check whether the game is over
 int checkGameState(Agent agents[],Graph g,int cycle,int maxCycles){
-    if(cycle >= maxCycles){
-	printf("GAME OVER: YOU LOSE - TIME IS UP\n");
-        return OVER;
-    }else{
-        return CONTINUE;
-    }
+   if(cycle >= maxCycles){
+      printf("GAME OVER: YOU LOSE - TIME IS UP\n");
+      return OVER;
+   }
+   int i;
+   Vertex tGeo = getCurrentLocation(agents[THIEF]);
+   for(i=1;i<=NUM_DETECTIVES;i++) {
+      Vertex dGPS = getCurrentLocation(agents[i]);
+      if (dGPS == tGeo) {
+         printf("D%d caught the thief ", i);
+         printf("in %s (%d)\n", getCityName(dGPS), dGPS);
+         printf("YOU WIN - THIEF CAUGHT!\n");
+         return WIN;
+      }
+   }
+   if (getDestination(agents[THIEF]) == tGeo) {
+      printf("T got away to %s (%d)\n", getCityName(tGeo), tGeo);
+      printf("GAME OVER: YOU LOSE - THIEF GOT TO GETAWAY\n");
+      return LOSE;
+   }
+   return CONTINUE;
 }
 
 //step through one cycle of the game
 int step(int cycle,Agent agents[],Graph g,int maxCycles){
 
-    int i;
-    cycle++;
-    for(i=0;i<=NUM_DETECTIVES;i++){
-       Edge  nextMove = getNextMove(agents[i],g);
-       makeNextMove(agents[i],nextMove);
+   int i;
+   cycle++;
+   for(i=0;i<=NUM_DETECTIVES;i++){
+      Edge  nextMove = getNextMove(agents[i],g);
+      makeNextMove(agents[i],nextMove);
 
-    }
-    display(cycle,agents,g);
-    int gameState = checkGameState(agents,g,cycle,maxCycles);
-    if(gameState == CONTINUE){
-        return cycle;
-    } else {
-        return gameState;
-    }
+   }
+   display(cycle,agents,g);
+   int gameState = checkGameState(agents,g,cycle,maxCycles);
+   if(gameState == CONTINUE){
+      return cycle;
+   } else {
+      return gameState;
+   }
 }
 
 
 //runs the game until the maximum number of cycles have completed
 int run(int cycle, int maxCycles, Agent agents[],Graph g){
-  while(cycle < maxCycles && cycle >= 0){
-       cycle = step(cycle,agents,g,maxCycles);
-  }
-  return cycle;
+   while(cycle < maxCycles && cycle >= 0){
+      cycle = step(cycle,agents,g,maxCycles);
+   }
+   return cycle;
 }
 
 
 void stats(int cycle, Agent agents[]){
-  int i;
-  printf("Hour %d\n",cycle);
-  for(i=0;i<=NUM_DETECTIVES;i++){
-    printAgent(agents[i]);
-  }
-  printf("\n");
+   int i;
+   printf("Hour %d\n",cycle);
+   for(i=0;i<=NUM_DETECTIVES;i++){
+      printAgent(agents[i]);
+   }
+   printf("\n");
 }
 
 void freeResources(Agent agents[], Graph g){
-    int i;
-    for(i=0;i<=NUM_DETECTIVES;i++){
-        destroyAgent(agents[i]);
-    }
-    destroyGraph(g);
+   int i;
+   for(i=0;i<=NUM_DETECTIVES;i++){
+      destroyAgent(agents[i]);
+   }
+   destroyGraph(g);
 }
 
 
 int main(int argc, char * argv[]){
-    Agent agents[NUM_DETECTIVES+1];
+   Agent agents[NUM_DETECTIVES+1];
 
-    if(argc < 4){
+   if(argc < 4){
       printf("Incorrect usage: please enter filename1 filename2 maxCycles\n");
       exit(0);
-    }
+   }
 
-    int maxCycles = atoi(argv[3]);
-    int cycle = 0;
-    int seed = time(NULL);
+   int maxCycles = atoi(argv[3]);
+   int cycle = 0;
+   int seed = time(NULL);
 
-    if(argc == 5)
-         seed = atoi(argv[4]);
-
-
-    srand(seed);
-
-    Graph g = readGraph(argv[1]);
-    #ifdef DEBUG
-        show(g);
-    #endif
-
-    initialiseAgents(argv[2],agents,maxCycles,g);
-    #ifdef DEBUG
-        stats(cycle,agents);
-    #endif
+   if(argc == 5)
+      seed = atoi(argv[4]);
 
 
+   srand(seed);
 
-    char command[MAXLINE];
-    printf("\nPOLICE ACADEMY 1927");
-    printf("\n\nRed alert! A thief is on the run.\n");
-    printf("Agents, to your cars. You have %d hours.\n\n",maxCycles);
-    display(cycle,agents,g);
+   Graph g = readGraph(argv[1]);
+#ifdef DEBUG
+   show(g);
+#endif
 
-    if(checkGameState(agents,g,cycle,maxCycles) != CONTINUE){
-        freeResources(agents,g);
-        return 0;
-    }
-    printf("Enter command: ");
-    scanf("%s",command);
+   initialiseAgents(argv[2],agents,maxCycles,g);
+#ifdef DEBUG
+   stats(cycle,agents);
+#endif
 
 
-    while (strcmp(command,"quit") != 0){
-        if(strcmp(command,"display") == 0){
-             display(cycle,agents,g);
-        }else if(strcmp(command,"run") == 0){
-             cycle = run(cycle,maxCycles,agents,g);
-        }else if(strcmp(command,"step") == 0){
-             cycle = step(cycle,agents,g,maxCycles);
-        }else if(strcmp(command,"stats") == 0){
-             stats(cycle,agents);
-        }else if(strcmp(command,"map") == 0){
-             show(g);
-             printf("\n");
-        }
-        if(cycle < 0) break;
-        printf("Enter command: ");
-        scanf("%s",command);
-    }
-    freeResources(agents,g);
-    return 0;
+
+   char command[MAXLINE];
+   printf("\nPOLICE ACADEMY 1927");
+   printf("\n\nRed alert! A thief is on the run.\n");
+   printf("Agents, to your cars. You have %d hours.\n\n",maxCycles);
+   display(cycle,agents,g);
+
+   if(checkGameState(agents,g,cycle,maxCycles) != CONTINUE){
+      freeResources(agents,g);
+      return 0;
+   }
+   printf("Enter command: ");
+   scanf("%s",command);
+
+
+   while (strcmp(command,"quit") != 0){
+      if(strcmp(command,"display") == 0){
+         display(cycle,agents,g);
+      }else if(strcmp(command,"run") == 0){
+         cycle = run(cycle,maxCycles,agents,g);
+      }else if(strcmp(command,"step") == 0){
+         cycle = step(cycle,agents,g,maxCycles);
+      }else if(strcmp(command,"stats") == 0){
+         stats(cycle,agents);
+      }else if(strcmp(command,"map") == 0){
+         show(g);
+         printf("\n");
+      }
+      if(cycle < 0) break;
+      printf("Enter command: ");
+      scanf("%s",command);
+   }
+   freeResources(agents,g);
+   return 0;
 }
 
